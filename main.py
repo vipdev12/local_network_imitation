53535a0230966fe3fab1e46383c392f9820e911c
Dinislam <aikenamuteshova@gmail.com>
2024-03-07 14:27:00 +0600

    v1

---
+++ main.py
@@ -0,0 +1,88 @@
+class Server:
+    """для описания работы серверов в сети
+    Соответственно в объектах класса Server должны быть локальные свойства:
+    buffer - список принятых пакетов (изначально пустой);
+    ip - IP-адрес текущего сервера.
+    """
+    _ip = 1
+
+    def __init__(self):
+        self.ip = Server._ip
+        self.buffer = []
+        Server._ip += 1
+
+    def send_data(self, data):
+        """для отправки информационного пакета data (объекта класса Data)
+        с указанным IP-адресом получателя (пакет отправляется роутеру и
+        сохраняется в его буфере - локальном свойстве buffer);
+        """
+        router.buffer.append(data)
+
+    def get_data(self):
+        """возвращает список принятых пакетов (если ничего принято не было,
+        то возвращается пустой список) и очищает входной буфер;
+        """
+        show = self.buffer[:]
+        self.buffer = []
+        return show
+
+    def get_ip(self):
+        """возвращает свой IP-адрес.
+        """
+        return self.ip
+
+
+class Router:
+    """для описания работы роутеров в сети (в данной задаче полагается один роутер).
+    И одно обязательное локальное свойство (могут быть и другие свойства):
+    buffer - список для хранения принятых от серверов пакетов (объектов класса Data).
+    """
+    def __init__(self):
+        self.linked_servers = {}
+        self.buffer = []
+
+    def link(self, server):
+        self.linked_servers[server.get_ip()] = server
+
+    def unlink(self, server):
+        """для отсоединения сервера server (объекта класса Server) от роутера
+        """
+        self.linked_servers.pop(server.get_ip())
+
+    def send_data(self):
+        """для отправки всех пакетов (объектов класса Data) из буфера роутера
+        соответствующим серверам (после отправки буфер должен очищаться)
+        """
+        for pack in self.buffer:
+            if pack.ip in self.linked_servers:
+                self.linked_servers[pack.ip].buffer.append(pack)
+        self.buffer = []
+
+
+
+class Data:
+    """для описания пакета информации
+    Наконец, объекты класса Data должны содержать, два следующих локальных свойства:
+    data - передаваемые данные (строка);
+    ip - IP-адрес назначения.
+    """
+    def __init__(self, data, ip):
+        self.data = data
+        self.ip = ip
+
+
+router = Router()
+sv_from = Server()
+sv_from2 = Server()
+router.link(sv_from)
+router.link(sv_from2)
+router.link(Server())
+router.link(Server())
+sv_to = Server()
+router.link(sv_to)
+sv_from.send_data(Data("Hello", sv_to.get_ip()))
+sv_from2.send_data(Data("Hello", sv_to.get_ip()))
+sv_to.send_data(Data("Hi", sv_from.get_ip()))
+router.send_data()
+msg_lst_from = sv_from.get_data()
+msg_lst_to = sv_to.get_data()
